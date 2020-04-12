e5ab2575b6f5f1949d0bbbc8535578f907e0d6dd
Vincent Law <vincentlaw@Vincents-MacBook-Pro.local>
2020-04-12 14:42:47 -0700

    d

--- app/index.py
+++ app/index.py
@@ -1,18 +1,18 @@
 from flask import Flask, Blueprint, request, render_template
 from app import api
+import os
+
 
 def create_app():
     """ Factory function to start application  """
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path='', static_folder='../client/build')
 
     app.url_map.strict_slashes = False
-    
+
     register_blueprints(app)
-    
-    
 
     return app
 
+
 def register_blueprints(app):
     app.register_blueprint(api.routes.main)
-    
