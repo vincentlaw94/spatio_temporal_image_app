b1de889911de8663dfb7a4c7f11e8970468a764a
Vincent Law <vincentlaw@Vincents-MacBook-Pro.local>
2020-04-12 14:57:30 -0700

    d

---
+++ client/build/precache-manifest.e550d0f971932b6b90425b2d79668a9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec3cfffde272a7abd99ededd0ce5e22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72d103eb790e064725c8",
+    "url": "/static/css/main.3b8a59bf.chunk.css"
+  },
+  {
+    "revision": "ef54968f25596811eab4",
+    "url": "/static/js/2.873a14ec.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.873a14ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72d103eb790e064725c8",
+    "url": "/static/js/main.362b1b40.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  },
+  {
+    "revision": "9aa58b1d86c1aaff1d5b8c22b6a705de",
+    "url": "/static/media/NoSignal.9aa58b1d.gif"
+  }
+]);
